82ad0788389205873156df42082240f7b44681f2
Benji Gogan <benji@Benjis-MacBook-Pro.local>
2020-07-06 00:24:22 +0100

    cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
 import Header from '../components/header'
-import 'bulma/css/bulma.css'
-
-// Or manually specify a host
-
-// export default () => <div>Hello world!</div>
 
 interface State { }
 interface Props { }
@@ -20,7 +15,7 @@ export default class Hello extends React.Component<State, Props> {
             <div>
                 <Header />
                 <h1>Home</h1>
-                <button className="button is-primary">Hi</button>
+                <button>Hi</button>
                 <Link to='/about/'>About</Link>
             </div>
         )
