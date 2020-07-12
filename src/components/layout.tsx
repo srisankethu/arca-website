7111c14033dc09d5232fda18c1796abdec137983
srisankethu <sanketh.uppalapati@research.iiit.ac.in>
2020-07-13 00:21:32 +0530

    Update landing page

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import "./layout.css"
-import { Grommet, Anchor, Box, Header, Footer, Text, Nav } from "grommet"
+import { Grommet, Anchor, Box, Header, Footer, Text, Nav, Button } from "grommet"
 import { grommet } from "grommet/themes"
 
 const Layout = ({ children }) => {
@@ -28,22 +28,30 @@ const Layout = ({ children }) => {
     >
       <Header background = "brand">
           <Nav direction="row" background="brand" pad="medium">
+              <img src={`logo.png`} width="200" height="60"/>
               <Anchor href = "#about" label = "What is ARCA?" />
               <Anchor href = "#articles" label = "Articles" />
               <Anchor href = "#projects" label = "Projects" />
               <Anchor href = "/#" label = "Submit Proposal" />
+              <Box pad="xsmall" direction ="row" border = {{ color: "white", size: 'small' }} round = "xsmall" gap="small">
+              <Box pad = "small" background = "status-ok" round = "full"/>
+              <Text>Online</Text>
+              </Box>
+              <Button primary href = "/#" label = "Join Us" />
           </Nav>
       </Header>
       <Box as="main" pad="medium" flex overflow="auto">
         {children}
       </Box>
       <Footer background="light-4" justify="center" pad="small">
+        <img src={`logo.png`} width="200" height="60"/>
         <Text textAlign="center" size="small">
           © {new Date().getFullYear()} ARCA DAO. All Rights reserved.
           {` `}
           <Anchor href="/#">Terms of Service</Anchor>
           {` | `}
           <Anchor href="/#">Privacy Policy</Anchor>
+          Follow us on <a href="/#"><img src={`facebook.png`}/></a><a href="/#"><img src={`twitter.png`}/></a>
         </Text>
       </Footer>
     </Grommet>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Box, Heading, Paragraph, Button } from "grommet"
+import { Box, Heading, Paragraph, Button, Avatar, Text } from "grommet"
+import {Checkmark} from "grommet-icons"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -16,11 +17,61 @@ const IndexPage = () => (
     </div>
         
     <div id="articles">
-        <Heading> Articles </Heading>
+        <Box align="center">
+            <Heading> Articles </Heading>
+            <Box direction = "row-responsive" gap = "xlarge">
+                <Box pad="small" border={{ color: "light-3"}}>
+                    <Image fit="cover"src="//v2.grommet.io/assets/Wilderpeople_Ricky.jpg"/>
+                    <Heading level="3"> Title of Article 1</Heading>
+                    <Box direction = "row" gap = "small">
+                        <Avatar size = "40px" src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80" />
+                        <Heading level="5"> John Doe </Heading>
+                    </Box>
+                    <Paragraph> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Paragraph>
+                </Box>
+                <Box pad = "small" border={{ color: "light-3"}}>
+                    <Image fit="cover" src="//v2.grommet.io/assets/Wilderpeople_Ricky.jpg"/>
+                    <Heading level="3"> Title of Article 2</Heading>
+                    <Box direction = "row" gap = "small">
+                        <Avatar size = "40px" src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80" />
+                        <Heading level="5"> John Doe </Heading>
+                    </Box>
+                    <Paragraph> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Paragraph>
+                </Box>
+                <Box pad = "small" border={{ color: "light-3"}}>
+                    <Image fit="cover" src="//v2.grommet.io/assets/Wilderpeople_Ricky.jpg"/>
+                    <Heading level="3"> Title of Article 3</Heading>
+                    <Box direction = "row" gap = "small">
+                        <Avatar size = "40px" src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80" />
+                        <Heading level="5"> John Doe </Heading>
+                    </Box>
+                    <Paragraph> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Paragraph>
+                </Box>
+            </Box>
+        </Box>
     </div>
        
     <div id="projects">
-        <Heading> Projects </Heading>
+        <Box align="center">
+            <Heading> Projects </Heading>
+            <Box direction = "row-responsive" gap = "xlarge">
+                <Box pad="small" border={{ color: "light-3"}}>
+                    <img src={`project1.png`} />
+                    <Heading level="3"> Feedweave </Heading>
+                    <Paragraph> A Decentralised Community Built Social Network </Paragraph>
+                </Box>
+                <Box pad = "small" border={{ color: "light-3"}}>
+                    <img src={`project2.png`} />
+                    <Heading level="3"> Arweave Reverse DNS Gateway </Heading>
+                    <Paragraph> Point a domain you own to your arweave DApp. </Paragraph>
+                </Box>
+                <Box pad = "small" border={{ color: "light-3"}}>
+                    <img src={`project3.png`} />
+                    <Heading level="3"> Permasnap </Heading>
+                    <Paragraph> Censorship Resistant Moments, embedded on the permaweb. </Paragraph>
+                </Box>
+            </Box>
+        </Box>
     </div>
         
     <div id="about">
@@ -33,33 +84,40 @@ const IndexPage = () => (
         <Heading> What is Permaweb? </Heading>
         <Paragraph> Arweave is a new type of storage that backs data with sustainable and perpetual endowments, allowing users and developers to truly store data forever - for the very first time.</Paragraph>
         <Paragraph> As a collectively owned hard drive that never forgets, Arweave allows us to remember and preserve valuable information, apps, and history indefinitely. By preserving history, it prevents others from rewriting it.</Paragraph>
-        <Button primary label="Visit ARWEAVE" href="https://arweave.org" target="_blank"/>
-        <Button primary label="Built on the PERMAWEB" href="https://arweaveapps.com" target="_blank"/>
+        <Box direction = "row" gap = "small">
+            <Button primary label="Visit ARWEAVE" href="https://arweave.org" target="_blank" color = "lightgreen"></Button>
+            <Button primary label="Built on the PERMAWEB" href="https://arweaveapps.com" target="_blank" color = "#1E90FF"></Button>
+        </Box>
     </div>
         
     <div id = "joinUs">
-        <Box>
-            <Heading> Why would you join us:</Heading>
-            <ul>
-                <li> Brainstorm  innovate ideas and build them using decentralized and open source tools.</li>
-                <li> Get your hands on Decentralized Finance by taking part of a democratic voting process  to ensure we put our DAI towards the right areas - whelther technology grants or operational tasks.</li>
-                <li> Potentially earn DAI through grants and helping ARCA with operational activities.</li>
-                <li> Learn the latest in decentralized technology, built around Arweave.</li>
-                <li> Network your skills with other developers, investors and blockchain experts.</li>
-                <li> Access to test servers and software for developing proof of concepts.</li>
-            </ul>
-        </Box>
-        <Box pad="medium" background="brand">
-            <Heading> Be part of ARCADAO</Heading>
-            <Paragraph> ARCA is calling all those passionate about technology and Arweave - whelther you are a dev, a network engineer, a social media guru, a writer, a leader, a ower, or even if even you just have the next big, decentralized idea.</Paragraph>
-            <Button primary label="Join us" />
+        <Box direction="row">
+            <Box width = "xlarge">
+                <Heading> Why would you join us:</Heading>
+                <ul>
+                    <li style="list-style-type:none"><Checkmark color="green"/> Brainstorm  innovate ideas and build them using decentralized and open source tools.</li>
+                    <li style="list-style-type:none"><Checkmark color="green"/> Get your hands on Decentralized Finance by taking part of a democratic voting process  to ensure we put our DAI towards the right areas - whelther technology grants or operational tasks.</li>
+                    <li style="list-style-type:none"><Checkmark color="green"/> Potentially earn DAI through grants and helping ARCA with operational activities.</li>
+                    <li style="list-style-type:none"><Checkmark color="green"/> Learn the latest in decentralized technology, built around Arweave.</li>
+                    <li style="list-style-type:none"><Checkmark color="green"/> Network your skills with other developers, investors and blockchain experts.</li>
+                    <li style="list-style-type:none"><Checkmark color="green"/> Access to test servers and software for developing proof of concepts.</li>
+                </ul>
+            </Box>
+            <Box pad="medium" background="brand">
+                <Heading> Be part of ARCADAO</Heading>
+                <Paragraph> ARCA is calling all those passionate about technology and Arweave - whelther you are a dev, a network engineer, a social media guru, a writer, a leader, a ower, or even if even you just have the next big, decentralized idea.</Paragraph>
+                <Button primary label="Join us" />
+            </Box>
         </Box>
     </div>
         
     <div>
+        <Box align="center">
+        <img src={`submitproposal.png`} />
         <Heading> Have a vision for the permaweb? </Heading>
         <Paragraph> We exist to fund development of anything that makes the permaweb better, anything that makes it easier to work with, and anything that brings more users in. If you have a cool idea you'd like to bring to life, and the time and skills to make it happen, check out our grant application form</Paragraph>
-        <Button primary label="Submit Proposal" />
+        <Button label="Submit Proposal" color="light-2"/>
+        </Box>
     </div>
   </Layout>
 )
